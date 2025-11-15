38d5351b9c0257d809852bbfc4dcd9e2a05e28bc
Miquel Gómez <miquelgc2003@gmail.com>
2025-11-15 11:24:13 +0100

    Ready for LLM prompting and doing LLM fine-tunning

--- app/src/data/dataloader.py
+++ app/src/data/dataloader.py
@@ -56,9 +56,11 @@ class TranslationDatasetLLM(Dataset):
         source = self.dataframe.iloc[idx][self.src_col]
         target = self.dataframe.iloc[idx][self.tgt_col]
         
+        # print(f"Source: {source}")
+        # print(f"Prefix: {self.task_prefix}")
         # Tokenize
         inputs = self.tokenizer(
-            self.task_prefix + source,
+            self.task_prefix + str(source),
             max_length=self.max_length,
             padding='max_length',
             truncation=True,
